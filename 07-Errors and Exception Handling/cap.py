a8bd98ba00f151c50165363f84ecc2644fc9fe48
Alexander Smith <alexander.smith@investec.co.uk>
2023-04-01 09:10:59 +0100

    push current progress

--- 07-Errors and Exception Handling/cap.py
+++ 07-Errors and Exception Handling/cap.py
@@ -1,2 +1,4 @@
+import string
 def cap_text(text):
-    return text.title()  # replace .capitalize() with .title()
+    # return text.capwords(se)  # replace .capitalize() with .title()
+    return string.capwords(text, sep=None)
